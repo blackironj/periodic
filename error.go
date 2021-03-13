c4c4f49cb7357703604ff90ddd0116528f277703
blackironj <blackironj@gmail.com>
2021-03-13 22:49:40 +0900

    Add RegisterTask function to Scheduler

--- error.go
+++ error.go
@@ -6,4 +6,5 @@ var (
 	ErrNoFunction          = errors.New("only function can be registered")
 	ErrNotMatchedNumParams = errors.New("the number of params is not matched")
 	ErrNotRegistered       = errors.New("it is not registered")
+	ErrDuplicated          = errors.New("task name duplicated")
 )
--- scheduler.go
+++ scheduler.go
@@ -14,24 +14,45 @@ const (
 )
 
 type scheduledTask struct {
-	task     task
-	interval time.Duration
-	ticker   *time.Ticker
-
+	task        *Task
+	interval    time.Duration
 	immediately bool
 	status      TaskStatus
 }
 
-//Scheduler struct keep task informations
+//Scheduler struct has task informations
 type Scheduler struct {
 	taskMap map[string]*scheduledTask
 	rwMutex sync.RWMutex
 }
 
+//RegisterTask register a task that runs periodically
+func (s *Scheduler) RegisterTask(taskName string, interval time.Duration, imediately bool, taskFunc interface{}, params ...interface{}) error {
+	s.rwMutex.Lock()
+	defer s.rwMutex.Unlock()
+
+	if _, ok := s.taskMap[taskName]; ok {
+		return ErrDuplicated
+	}
+
+	newTask, err := NewTask(taskFunc, params)
+	if err != nil {
+		return err
+	}
+
+	s.taskMap[taskName] = &scheduledTask{
+		task:        newTask,
+		interval:    interval,
+		immediately: imediately,
+		status:      Stopped,
+	}
+	return nil
+}
+
 //GetTaskStatus returns status of specific task
 func (s *Scheduler) GetTaskStatus(taskName string) (TaskStatus, error) {
 	s.rwMutex.RLock()
-	defer s.rwMutex.Unlock()
+	defer s.rwMutex.RUnlock()
 
 	if task, ok := s.taskMap[taskName]; ok {
 		return task.status, nil
