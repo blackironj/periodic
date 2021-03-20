4462a03ee69cb8e7e1f2d83ac892a897a55b47eb
blackironj <blackironj@gmail.com>
2021-03-20 18:39:49 +0900

    Fix test data race

--- scheduler_test.go
+++ scheduler_test.go
@@ -1,6 +1,7 @@
 package periodic
 
 import (
+	"sync/atomic"
 	"testing"
 	"time"
 
@@ -8,35 +9,45 @@ import (
 )
 
 func TestScheduler(t *testing.T) {
-	var firstVal, secondVal, thirdVal int
+	var firstVal, secondVal, thirdVal int32
+	testFunc := func(val *int32) {
+		time.Sleep(time.Millisecond * 50)
+		atomic.AddInt32(val, 1)
+	}
+
 	jobs := []struct {
 		taskName    string
 		interval    time.Duration
 		taskFunc    interface{}
+		taskParams  *int32
 		expectedErr error
 	}{
 		{
 			taskName:    "first",
 			interval:    time.Millisecond * 500,
-			taskFunc:    func() { firstVal++ },
+			taskFunc:    testFunc,
+			taskParams:  &firstVal,
 			expectedErr: nil,
 		},
 		{
 			taskName:    "first",
 			interval:    time.Millisecond * 500,
-			taskFunc:    func() { firstVal++ },
+			taskFunc:    testFunc,
+			taskParams:  &firstVal,
 			expectedErr: ErrDuplicated,
 		},
 		{
 			taskName:    "second",
-			interval:    time.Millisecond * 200,
-			taskFunc:    func() { secondVal++ },
+			interval:    time.Millisecond * 1000,
+			taskFunc:    testFunc,
+			taskParams:  &secondVal,
 			expectedErr: nil,
 		},
 		{
 			taskName:    "third",
-			interval:    time.Millisecond * 1000,
-			taskFunc:    func() { thirdVal++ },
+			interval:    time.Millisecond * 2000,
+			taskFunc:    testFunc,
+			taskParams:  &thirdVal,
 			expectedErr: nil,
 		},
 	}
@@ -46,7 +57,7 @@ func TestScheduler(t *testing.T) {
 	t.Run("Register tasks to scheduler", func(t *testing.T) {
 		for _, j := range jobs {
 			j := j
-			newTask, err := NewTask(j.taskFunc)
+			newTask, err := NewTask(j.taskFunc, j.taskParams)
 			assert.NoError(t, err)
 
 			err = scheduler.RegisterTask(j.taskName, j.interval, newTask)
@@ -55,31 +66,33 @@ func TestScheduler(t *testing.T) {
 	})
 
 	scheduler.Run()
-	time.Sleep(time.Millisecond * 1850)
+	time.Sleep(time.Millisecond * 2900)
 	scheduler.Stop()
 
-	time.Sleep(time.Millisecond * 500)
+	time.Sleep(time.Millisecond * 100)
 	t.Run("Check scheduler runs accurately", func(t *testing.T) {
 		cases := []struct {
-			expected int
-			actual   int
+			expected int32
+			actual   int32
 		}{
 			{
-				expected: 4,
-				actual:   firstVal,
+				expected: 6,
+				actual:   atomic.LoadInt32(&firstVal),
 			},
 			{
-				expected: 10,
-				actual:   secondVal,
+				expected: 3,
+				actual:   atomic.LoadInt32(&secondVal),
 			},
 			{
 				expected: 2,
-				actual:   thirdVal,
+				actual:   atomic.LoadInt32(&thirdVal),
 			},
 		}
+
 		for _, tc := range cases {
 			tc := tc
 			assert.Equal(t, tc.expected, tc.actual)
 		}
+
 	})
 }
