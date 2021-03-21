229d9cd432ee137442961733431d189c6f11fc09
blackironj <blackironj@gmail.com>
2021-03-21 13:56:43 +0900

    Modify scheduler test code

--- scheduler_test.go
+++ scheduler_test.go
@@ -1,98 +1,40 @@
 package periodic
 
 import (
-	"sync/atomic"
+	"sync"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestScheduler(t *testing.T) {
-	var firstVal, secondVal, thirdVal int32
-	testFunc := func(val *int32) {
-		time.Sleep(time.Millisecond * 50)
-		atomic.AddInt32(val, 1)
-	}
+func TestSchedulerRun(t *testing.T) {
+	interval := time.Millisecond * 300
+	intervalPlusTiny := interval + time.Millisecond*30
 
-	jobs := []struct {
-		taskName    string
-		interval    time.Duration
-		taskFunc    interface{}
-		taskParams  *int32
-		expectedErr error
-	}{
-		{
-			taskName:    "first",
-			interval:    time.Millisecond * 500,
-			taskFunc:    testFunc,
-			taskParams:  &firstVal,
-			expectedErr: nil,
-		},
-		{
-			taskName:    "first",
-			interval:    time.Millisecond * 500,
-			taskFunc:    testFunc,
-			taskParams:  &firstVal,
-			expectedErr: ErrDuplicated,
-		},
-		{
-			taskName:    "second",
-			interval:    time.Millisecond * 1000,
-			taskFunc:    testFunc,
-			taskParams:  &secondVal,
-			expectedErr: nil,
-		},
-		{
-			taskName:    "third",
-			interval:    time.Millisecond * 2000,
-			taskFunc:    testFunc,
-			taskParams:  &thirdVal,
-			expectedErr: nil,
-		},
-	}
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+
+	testTask, err := NewTask(func() { wg.Done() })
+	assert.NoError(t, err)
 
 	scheduler := NewScheduler()
+	err = scheduler.RegisterTask("test", interval, testTask)
+	assert.NoError(t, err)
 
-	t.Run("Register tasks to scheduler", func(t *testing.T) {
-		for _, j := range jobs {
-			j := j
-			newTask, err := NewTask(j.taskFunc, j.taskParams)
-			assert.NoError(t, err)
+	select {
+	case <-time.After(intervalPlusTiny):
+		//No error
+	case <-wait(wg):
+		t.Fatal()
+	}
+}
 
-			err = scheduler.RegisterTask(j.taskName, j.interval, newTask)
-			assert.Equal(t, j.expectedErr, err)
-		}
-	})
-
-	scheduler.Run()
-	time.Sleep(time.Millisecond * 2700)
-	scheduler.Stop()
-
-	time.Sleep(time.Millisecond * 100)
-	t.Run("Check scheduler runs accurately", func(t *testing.T) {
-		cases := []struct {
-			expected int32
-			actual   int32
-		}{
-			{
-				expected: 6,
-				actual:   atomic.LoadInt32(&firstVal),
-			},
-			{
-				expected: 3,
-				actual:   atomic.LoadInt32(&secondVal),
-			},
-			{
-				expected: 2,
-				actual:   atomic.LoadInt32(&thirdVal),
-			},
-		}
-
-		for _, tc := range cases {
-			tc := tc
-			assert.Equal(t, tc.expected, tc.actual)
-		}
-
-	})
+func wait(wg *sync.WaitGroup) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		wg.Wait()
+		ch <- struct{}{}
+	}()
+	return ch
 }
